ebb8b7583d2e452289a38425606c4d88428bea13
Simone <56943425+mizardblack@users.noreply.github.com>
2020-03-06 13:50:45 -0500

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,75 @@
+var mymap = L.map('mapid').setView([40.734121, -73.987255], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+//pin
+var marker = L.marker([40.700065, -73.950176]).addTo(mymap)
+    .bindTooltip("my tooltip text").openTooltip();;
+
+//layers group
+var USQ = L.marker([40.734641, -73.990259]).bindPopup(`This is Union Square.`),
+    school = L.marker([40.734706, -73.994637]).bindPopup(`This is my school.`),
+    DD = L.marker([40.71755, -73.963373]).bindPopup(`This is Daddy's home.`),
+    home = L.marker([40.731700, -73.992405]).bindPopup(`This is my home.`);
+
+var cities = L.layerGroup([USQ, school, DD, home]).addTo(mymap);
+
+var overlayMaps = {
+    "Cities": cities
+};
+
+//popups
+marker.bindPopup("<b>Flushing Ave</b><br>Theres no security camera here.").openPopup();
+
+//click event
+// var popup = L.popup();
+
+// function onMapClick(e) {
+//     console.log(`[${e.latlng.lat}, ${e.latlng.lng}]`);
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent(`You clicked the map at [${e.latlng.lat}, ${e.latlng.lng}]`)
+//         .openOn(mymap);
+
+// }
+
+// mymap.on('click', onMapClick);
+
+//edit button
+var edit_button = document.getElementById("edit");
+
+function editClick(e) {
+
+}
+
+//create new 
+// var create_button = document.getElementById("create");
+
+
+function create(e) {
+    let create_button = document.getElementById("create");
+
+    // var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
+    console.log(create_button.checked);
+    if (create_button.checked) {
+        let no_camera = L.icon({
+            iconUrl: 'img/nocamera.png',
+            iconSize: [21, 27],
+            iconAnchor: [11, 27],
+            popupAnchor: [-3, -76],
+        });
+        L.marker([e.latlng.lat, e.latlng.lng], { icon: no_camera }).addTo(mymap);
+    }
+}
+
+mymap.on('click', create);
+
+
